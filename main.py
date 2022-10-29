a64592876612336c33c86c3c0e019d6106f2bd5a
hasindusithmin <hasindusithmin64@gmail.com>
2022-10-30 00:09:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import re
+import textwrap
 from io import BytesIO
 from typing import Optional
 from fastapi import FastAPI,HTTPException
 from fastapi.responses import RedirectResponse
 from fastapi.responses import StreamingResponse
-from PIL import Image, ImageDraw,ImageColor
+from PIL import Image, ImageDraw,ImageColor,ImageFont
 app = FastAPI(
     title="PlaceholderAPI",
     version="0.1.0"
@@ -15,17 +16,27 @@ def redirect():
     return RedirectResponse('/docs')
 
 @app.get('/{width}/{height}/{color}')
-def generate_image(width:int,height:int,color:str,text:Optional[str]=None):
+def generate_image(
+    width:int,
+    height:int,
+    color:str,
+    fontsize:Optional[int]=18,
+    text:Optional[str]=None
+    ):
     if not re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', f'#{color}'):
         raise HTTPException(status_code=400,detail=f"#{color} is not a valid hex color code")
     W, H = (width,height)
     rgb = ImageColor.getcolor(f'#{color}', "RGB")
     image = Image.new("RGB", (W, H), rgb)
     if text is not None:
-        text = text.replace(',','\n')
+        para = textwrap.wrap(text, width=15)
+        current_h, pad = 50, 10
         draw = ImageDraw.Draw(image)
-        w, h = draw.textsize(text)
-        draw.multiline_text(((W-w)/2,(H-h)/2), text, fill=(0, 0, 0))
+        font = ImageFont.truetype("arial.ttf", fontsize)
+        for line in para:
+            w, h = draw.textsize(line, font=font)
+            draw.text(((W - w) / 2, current_h), line, font=font, fill=(169,169,169))
+            current_h += h + pad
     buffer = BytesIO()
     image.save(buffer,format='PNG')
     buffer.seek(0)
